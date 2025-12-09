19b3af62a65e5d2c3d5e812448f08b398269a55b
ivon0007 <stupapupa563@gmail.com>
2025-12-09 11:19:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 2, 9, 1 };
+
+        int minValue1 = numbers.Min();
+        Console.WriteLine($"Минимальное число (способ 1): {minValue1}");
+
+        // Ожидаемый результат: 1
+    }
+}
